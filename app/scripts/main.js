7d3c1e17e5360f7d90ecbdc631294926abe921f3
Daniel Vivar <jdvivar@gmail.com>
2016-10-11 16:25:09 +0100

    build

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,32 +12,44 @@ document.addEventListener('DOMContentLoaded', function() {
   handleOrientationChange(mql);
 
   function handleOrientationChange(mql) {
+    var trackedEvent = 'click';
+
     if (mql.matches) {
-      var trackedEvent = 'click';
       // Mobile view
+
+      // track event and callback
       $('.js-img-zoom').on(trackedEvent, toggleZoom);
-      var ezpData = $('.zoomed img').data('ezPlus');
+
+      // clean zoom plugin data
+      var ezpData = $('.js-img-zoom > img').data('ezPlus');
       ezpData ? ezpData.destroy() : null;
     } else {
       // Desktop view
+
+      // untrack event
       $('.js-img-zoom').off(trackedEvent);
+
+      // init zoom plugin with options
       $('.js-img-zoom > img').ezPlus(ezpOptions);
     }
   }
 
   // Toggle zoom
+
   var zoom = false;
+  var animationTime = 600;
+
   function toggleZoom (e) {
     var el = $(e.target);
     if (zoom) {
       el.css('width','100%');
     } else {
       el.css('width','500%');
-      el.css('transition', 'all .6s');
+      el.css('transition', 'all ' + animationTime/1000 + 's');
       el.parent().animate({
         scrollTop: el.height() * 5 / 2.5,
         scrollLeft: el.width() * 5 / 2.5
-      }, 600);
+      }, animationTime);
     }
     zoom = !zoom;
   }
