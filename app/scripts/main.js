4ab35b3ba51d2425803f4ec4f894350bbe410685
Daniel Vivar <jdvivar@gmail.com>
2016-10-14 10:26:47 +0100

    Fixes to code when handling mobile/desktop changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,43 +6,65 @@ document.addEventListener('DOMContentLoaded', function() {
     zoomWindowOffsetY: -2
   };
 
+  var zoomedElement = $('.js-img-zoom');
+  var instructionsDelay = 3000;
+  var isZoom = false;
+  var animationTime = 600;
+
   // responsive behaviour
   var mql = window.matchMedia('(max-width: 768px)');
   mql.addListener(handleOrientationChange);
-  handleOrientationChange(mql);
+  // handleOrientationChange(mql);
 
   function handleOrientationChange(mql) {
     var trackedEvent = 'click';
+    var previousZoomedElement = zoomedElement;
+    zoomedElement = $('.js-img-zoom');
+
+    // clean zoom plugin data
+    var ezpData;
+    ezpData = zoomedElement.children('img').first().data('ezPlus');
+    ezpData ? ezpData.destroy() : null;
+    ezpData = previousZoomedElement.children('img').first().data('ezPlus');
+    ezpData ? ezpData.destroy() : null;
+
+    zoomOut(zoomedElement.children('img').first());
+    setImageContainerHeight(zoomedElement, animationTime);
 
     if (mql.matches) {
       // Mobile view
 
-      setImageContainerHeight();
+      hideInstructions($('.js-instructions'), instructionsDelay);
+
+      // untrack event
+      previousZoomedElement.off(trackedEvent);
+      zoomedElement.off(trackedEvent);
 
       // track event and callback
-      $('.js-img-zoom').on(trackedEvent, toggleZoom);
+      zoomedElement.on(trackedEvent, toggleZoom);
 
-      // clean zoom plugin data
-      var ezpData = $('.js-img-zoom > img').data('ezPlus');
-      ezpData ? ezpData.destroy() : null;
     } else {
       // Desktop view
 
-      hideInstructions();
+      // hide instructions
+      hideInstructions($('.js-instructions'), 0);
 
       // untrack event
-      $('.js-img-zoom').off(trackedEvent);
+      zoomedElement.off(trackedEvent);
+      previousZoomedElement.off(trackedEvent);
 
-      // init zoom plugin with options
-      $('.js-img-zoom > img').ezPlus(ezpOptions);
+      // init zoom plugin
+      startEZP(ezpOptions);
     }
   }
 
-  // Toggle zoom
+  // function to start zoom plugin (desktop)
+  function startEZP(options) {
+    zoomedElement.children('img').first().ezPlus(options);
+  }
 
-  var isZoom = false;
-  var animationTime = 600;
 
+  // function to toggle zoom natively (mobile)
   function toggleZoom(e) {
     var el = $(e.target);
     if (isZoom) {
@@ -52,14 +74,16 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 
+  // function to zoom out natively (mobile)
   function zoomOut(el) {
     isZoom = false;
     el.css('width', '100%');
   }
 
+  // function to zoom in natively
   function zoomIn(el) {
     isZoom = true;
-    hideInstructions();
+    hideInstructions($('.js-instructions'), 0);
     el.css('width', '500%');
     el.css('transition', 'all ' + animationTime / 1000 + 's');
     el.parent().animate({
@@ -68,14 +92,14 @@ document.addEventListener('DOMContentLoaded', function() {
     }, animationTime);
   }
 
-  function hideInstructions() {
-    var instructions = $('.js-img-zoom .js-instructions');
-    $(instructions).remove();
+  // function to remove from DOM instructions element
+  function hideInstructions(instructions, delay) {
+    instructions.css('animation-delay', delay / 1000 +'s');
+    setTimeout(function(){ $(instructions).remove(); }, delay + animationTime);
   }
 
-  // thumbnail list
-
-  $('.js-select-this-img').click(function(e){
+  // thumbnail list behaviour
+  $('.js-select-this-img').click(function(){
     var self = this;
 
     // remove active state from all thumbnails
@@ -85,23 +109,36 @@ document.addEventListener('DOMContentLoaded', function() {
     $(self).addClass('active');
 
     // set main image to the one selected
-    $('.js-img-zoom img').attr('src',$(self).children('img').attr('src'));
+    zoomedElement.children('img').first().attr('src',$(self).children('img').attr('src'));
 
     // set img container height according to contents within
-    // setImageContainerHeight();
-    setTimeout(function(){ setImageContainerHeight(); }, isZoom ? animationTime : 0);
+    setImageContainerHeight(zoomedElement, isZoom ? animationTime : 0);
 
     // force zoom out
-    zoomOut($('.js-img-zoom img'));
+    zoomOut(zoomedElement.children('img').first());
 
-    // restart
-    // startZoom($('.js-img-zoom'));
+    // force handle proper zoom method
+    handleOrientationChange(mql)
   });
 
-  function setImageContainerHeight() {
-    var containerHeight = parsePx($('.js-img-zoom img').css('height')) + parsePx($('.js-img-zoom').css('padding-bottom')) + parsePx($('.js-img-zoom').css('padding-top')) + parsePx($('.js-img-zoom').css('border-top-width')) + parsePx($('.js-img-zoom').css('border-bottom-width'));
-    $('.js-img-zoom').css('height',containerHeight);
+  // choose first image on list
+  $('.js-select-this-img').first().click();
+
+  // function to set image container height to the image height plus paddings and borders
+  function setImageContainerHeight(container, delay) {
+    setTimeout(function(){
+      // only execute if image is loaded
+      if (container.children('img').first().css('height') === 0) {
+        setImageContainerHeight(container, delay);
+        return false;
+      }
+
+      var containerHeight = parsePx(container.children('img').first().css('height')) + parsePx(container.css('padding-bottom')) + parsePx(container.css('padding-top')) + parsePx(container.css('border-top-width')) + parsePx(container.css('border-bottom-width'));
+      container.css('height',containerHeight);
+    }, delay);
   }
+
+  // function to convert size from css in px to a correctly rounded integer
   function parsePx(px) {
     return Math.round(parseFloat(px))
   }
